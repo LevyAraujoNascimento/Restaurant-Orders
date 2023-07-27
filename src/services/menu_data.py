b4c25a7f8a63ca088acf290b75d01f8fb5ee9022
Levy Araujo <levy4raujo@gmail.com>
2023-07-27 15:44:32 -0300

    [Req 03 concluído]
    Criando Menu Data.

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,34 @@
 # Req 3
+import csv
+from models.ingredient import Ingredient
+from models.dish import Dish
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.readingFile(source_path)
+
+    def readingFile(self, source_path):
+        menu = {}
+        with open(source_path, encoding="utf-8") as file:
+            header, *data = csv.reader(file)
+            for name, price, ingredient, num in data:
+                try:
+                    if menu[name]:
+                        menu[name].add_ingredient_dependency(
+                            Ingredient(ingredient), int(num)
+                        )
+                except KeyError:
+                    menu[name] = Dish(name, float(price))
+                    menu[name].add_ingredient_dependency(
+                        Ingredient(ingredient), int(num)
+                    )
+        self.settingDishes(menu)
+
+    def settingDishes(self, menu):
+        for dish in menu:
+            self.dishes.add(menu[dish])
+
+
+x = MenuData('data/menu_base_data.csv')
